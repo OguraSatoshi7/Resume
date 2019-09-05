62048c8a0707fc3ce63c2430c7e1aaede821d48d
OguraSatoshi7 <52971070+OguraSatoshi7@users.noreply.github.com>
2019-09-05 11:41:14 -0700

    Add files via upload

---
+++ Portfolio_905/js/main.js
@@ -0,0 +1,48 @@
+function Topnav() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+function FactorialCal(){
+	let Sum=1;
+	let number=document.getElementById("numberin").value;
+	if(number<1){
+		alert("Natural Number Only");
+		void(0);
+
+	}else{
+	for(number;number>0;number-=1){
+	Sum=Sum*number;
+	}
+	document.getElementById("factorial").innerHTML=Sum
+	}
+}
+
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
